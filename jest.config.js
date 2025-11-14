dc35d6579b858106f522671dd4d19abc6200c1dd
Chiman2937 <cyga2933@gmail.com>
2025-11-14 23:22:25 +0900

    test: Button 컴포넌트 jest test(Codecov comment 동작 확인용)
    - Button 컴포넌트 임시 등록
    - Button test 임시 등록

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,7 @@ const config = {
   testEnvironment: 'jsdom',
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
